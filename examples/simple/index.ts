ab795f7858600dd0a675edf7a3ea0e98ac978805
Stuart Attenborrow <stuarta0@gmail.com>
2021-11-03 14:50:30 +1100

    Added composite heightmap resource

--- examples/simple/index.ts
+++ examples/simple/index.ts
@@ -2,19 +2,49 @@ import "core-js/stable";
 import "regenerator-runtime/runtime";
 import "cesiumSource/Widgets/widgets.css";
 import "./main.css";
-const Cesium: any = require("cesiumSource/Cesium");
+const Cesium = require("cesiumSource/Cesium");
 // Import @types/cesium to use along with CesiumJS
 import { Viewer, Ion, IonResource, createWorldTerrain } from "cesium";
-import TerrainProvider from "../../dist";
+import TerrainProvider, { MartiniTerrainProvider, DefaultHeightmapResource, MapboxTerrainResource, CompositeHeightmapResource } from "../../dist";
+
+// const terrainResource = new DefaultHeightmapResource({
+//   url: "http://localhost:8080/public/terrain/{z}/{x}/{reverseY}.png",
+//   skipOddLevels: false,
+//   maxZoom: 18,
+//   decodeRgb: (r, g, b, a) => a < 255 ? 0 : (r * 256 * 256) * 0.001 + (g * 256.0) * 0.001 + b * 0.001 - 11200.0,
+// });
 
 // @ts-ignore
-const terrainProvider = new TerrainProvider({
+const terrainResource = new CompositeHeightmapResource({
+  urls: [
+    "http://localhost:8080/public/terrain1/{z}/{x}/{reverseY}.png",
+    "http://localhost:8080/public/terrain2/{z}/{x}/{reverseY}.png",
+    "http://localhost:8080/public/terrain3/{z}/{x}/{reverseY}.png",
+    "http://localhost:8080/public/terrain4/{z}/{x}/{reverseY}.png",
+  ],
+  skipOddLevels: false,
+  maxZoom: 18,
+  decodeRgb: (r, g, b, a) => a < 255 ? 0 : (r * 256 * 256) * 0.001 + (g * 256.0) * 0.001 + b * 0.001 - 11200.0,
+});
+
+const terrainProvider = new MartiniTerrainProvider({
+  resource: terrainResource,
   requestVertexNormals: false,
   requestWaterMask: false,
-  accessToken: process.env.MAPBOX_API_TOKEN,
-  skipOddLevels: false,
-  highResolution: false,
 });
+
+
+// const terrainResource = new MapboxTerrainResource({
+//   accessToken: process.env.MAPBOX_API_TOKEN,
+//   skipOddLevels: false,
+//   highResolution: false,
+// });
+
+// const terrainProvider = new TerrainProvider({
+//   resource: terrainResource,
+//   requestVertexNormals: false,
+//   requestWaterMask: false,
+// });
 
 let satellite = new Cesium.MapboxImageryProvider({
   mapId: "mapbox.satellite",
@@ -28,10 +58,10 @@ var opts = {
   //   style: Cesium.IonWorldImageryStyle.AERIAL,
   // }),
   // @ts-ignore
-  skyBox: false as false,
+  skyBox: false,
   baseLayerPicker: false,
   geocoder: false,
-  skyAtmosphere: false as false,
+  skyAtmosphere: false,
   animation: false,
   timeline: false,
   // Makes cesium not render high fps all the time
@@ -50,6 +80,11 @@ document.body.appendChild(g);
 
 var clat = -21.133786;
 var clon = 14.5481193;
+var zoom = 3000;
+
+clat = -20.5823678;
+clon = 117.1834165;
+zoom = 1000;
 
 const rect = Cesium.Rectangle.fromDegrees(
   clon - 0.01,
@@ -71,12 +106,12 @@ var viewer = new Cesium.Viewer(domID, opts);
 viewer.extend(Cesium.viewerCesiumInspectorMixin);
 viewer.scene.debugShowFramesPerSecond = true;
 
-var extent = Cesium.Cartesian3.fromDegrees(clon, clat - 0.3, 8000);
+var extent = Cesium.Cartesian3.fromDegrees(clon, clat - 0.015, zoom);
 viewer.camera.setView({
   destination: extent,
   orientation: {
     heading: Cesium.Math.toRadians(0), // east, default value is 0.0 (north)
-    pitch: Cesium.Math.toRadians(-15), // default value (looking down)
+    pitch: Cesium.Math.toRadians(-30), // default value (looking down)
     roll: 0.0, // default value
   },
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import DefaultHeightmapResource from './heightmap-resource'
 import MapboxTerrainResource from './mapbox-resource'
+import { CompositeHeightmapResource } from './composite-resource'
 import MapboxTerrainProvider, { MartiniTerrainProvider } from './terrain-provider'
 
 export default MapboxTerrainProvider
 export {
     MartiniTerrainProvider,
     DefaultHeightmapResource,
-    MapboxTerrainResource
+    MapboxTerrainResource,
+    CompositeHeightmapResource
 }
